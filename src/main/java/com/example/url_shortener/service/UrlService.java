737651b0706fe6d1f26323dea37a8eeae5afb903
Harsh <iknowharsh2024@gmail.com>
2026-09-24 14:22:16 +0530

    Add Radis Caching

--- src/main/java/com/example/url_shortener/service/UrlService.java
+++ src/main/java/com/example/url_shortener/service/UrlService.java
@@ -8,6 +8,7 @@ import com.example.url_shortener.util.Base62Encoder;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Service
@@ -59,7 +60,7 @@ public class UrlService {
         String originalUrl = url.getOriginalUrl();
 
         // Store result in Redis
-        redisTemplate.opsForValue().set(shortCode, originalUrl);
+        redisTemplate.opsForValue().set(shortCode, originalUrl, Duration.ofMinutes(10));
 
         System.out.println("Redis Cache MISS - Date loaded from MySQL");
 
